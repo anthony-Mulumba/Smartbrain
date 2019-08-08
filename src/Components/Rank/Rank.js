2def936ef39c07eb436d43b1f20e5688899cb028
Mulumba <amulumba@vodacom.cd>
2019-08-08 09:35:21 +0100

    Adding react particles

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,19 @@
+//jshint6 version:6
+import React from 'react';
+
+
+const Rank = () => {
+  return (
+    <div >
+      <div className='white f3'>
+        {'Anthony, your current rank is...'}
+      </div>
+
+      <div className='white f1'>
+        {'#5'}
+      </div>
+
+    </div>
+  )
+}
+export default Rank;
